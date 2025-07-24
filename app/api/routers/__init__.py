3fad350f413b43fa6ebf2d63d8f722d8a0ddaa65
1DelFin1 <defario777@mail.ru>
2025-07-24 13:40:42 +0500

    add router for upload documents

--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.routers import users, auth
+from app.api.routers import users, auth, documents
 
 router = APIRouter()
 router.include_router(users.router)
 router.include_router(auth.router)
+router.include_router(documents.router)
 
 __all__ = ("router",)
---
+++ app/api/routers/documents.py
@@ -0,0 +1,21 @@
+from uuid import UUID
+
+from fastapi import APIRouter, UploadFile, Depends
+
+from app.api.deps import SessionDep, get_broker
+from app.api.utils import FileManager
+
+
+router = APIRouter(tags=["documents"], prefix="/document")
+
+
+@router.post("/upload")
+async def upload_file(
+    session: SessionDep,
+    file: UploadFile,
+    user_id: UUID,
+    broker_mq=Depends(get_broker),
+):
+    manager = FileManager(file, user_id)
+    await manager.execute_task(session, broker_mq)
+    return {"doc_id": manager.doc_id, "status": "queued"}
